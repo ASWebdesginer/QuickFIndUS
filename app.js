3b0febabcc76a4dc2b76b7bdd6d0ecaf7b7dafd5
SAS developer <69052359+ASWebdesginer@users.noreply.github.com>
2023-08-29 21:28:55 +0500

    new update

--- app.js
+++ app.js
@@ -4,5 +4,30 @@ jQuery(document).ready(function($){
     })
     $(".hamburger").click(function(){
         $(".mobilemneu").show();
-    })
-})
+    });
+    $(".btnforme").click(function(){
+        $(".secondstep").show();
+        $(".firststep").hide();
+    });
+    // Hide all steps except the first one
+  
+  // Step 1: Next button click event
+  $(".btnforme").click(function() {
+    $(this).siblings("#amount").hide();
+    $(this).siblings("#email").show();
+    $(this).siblings("#submitbtn").show();
+    $(this).hide();
+  });
+
+  
+//   // Step 2: Submit button click event
+//   $("#submitForm").click(function() {
+//     var selectedOption = $("#selectField").val();
+//     var emailAddress = $("#emailField").val();
+    
+//     // Perform form validation or data processing here
+    
+//     // For this example, just showing the selected option and email
+//     alert("Selected Option: " + selectedOption + "\nEmail: " + emailAddress);
+//   });
+});
